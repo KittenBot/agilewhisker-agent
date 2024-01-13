2314836b6c94e3a2505d8346967d65298e601839
G-H-OME <1120314609@qq.com>
2024-01-13 14:39:22 +0800

    完善加速度计控制鼠标函数

--- src/jd_pcevent.ts
+++ src/jd_pcevent.ts
@@ -2,8 +2,6 @@ import { jdunpack, JDServiceServer } from 'jacdac-ts';
 import robot from '@jitsi/robotjs';
 import { exec } from 'child_process';
 
-robot.setMouseDelay(0.001)
-
 const SRV_PC_EVENT = 0x113d0987;
 const CMD_OPEN_URL = 0x80;
 const CMD_OPEN_APP = 0x81;
@@ -20,7 +18,7 @@ class PCEvent extends JDServiceServer {
         this.addCommand(CMD_OPEN_APP, this.handleOpenApp.bind(this));
         this.addCommand(CMD_SEND_TXT, this.handleSendText.bind(this));
         this.addCommand(CMD_RUN_SCRIPT, this.handleRunScript.bind(this));
-        this.addCommand(CMD_MOVE_MOUSE, this.handleMoveMouse.bind(this));
+        this.addCommand(CMD_MOVE_MOUSE, this.handleAccelMoveMouse.bind(this));
         this.addCommand(CMD_CLICK_MOUSE, this.handleClickMouse.bind(this));
     }
 
@@ -49,16 +47,17 @@ class PCEvent extends JDServiceServer {
         console.log("run script", script);
     }
 
-    handleMoveMouse(pkt: any): void {
+    handleAccelMoveMouse(pkt: any): void {
+        //加速度计控制鼠标移动
+        robot.setMouseDelay(0.1)
         const [position] = jdunpack(pkt.data, "s")
         const posArr = position.split(',')
         const moveX = parseFloat(posArr[0])
         const moveY = parseFloat(posArr[1])
         const screen = robot.getScreenSize()
-        
-        const screenX = (moveX+65535)*(screen.width/(65535*2))
-        const screenY = (moveY+65535)*(screen.height/(65535*2))
-        robot.moveMouseSmooth(screenX,screenY,0.01)
+        const targetX = Math.floor((moveX + 65535) / 131070 * screen.width);
+        const targetY = Math.floor((moveY + 65535) / 131070 * screen.height);
+        robot.moveMouseSmooth(targetX,targetY,0.001)
     }
 
     handleClickMouse(pkt: any): void {
