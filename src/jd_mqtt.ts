806705a6dfb4e08795afc556e8017029cc84004a
Si-ege <1660119581@qq.com>
2024-02-23 11:28:20 +0800

    feat: mqtt 服务

--- src/jd_mqtt.ts
+++ src/jd_mqtt.ts
@@ -7,20 +7,19 @@ interface Options {
     host?: string;
     topic?: string;
 }
-
-class MQTTServer extends CloudAdapterServer {
+const SRV_PC_MONITOR = 0x113d0988;
+class MQTTServer extends JDServiceServer {
     private server: net.Server;
     private topic: string;
     static broker: Aedes;
     private client: MQTTClient;
+    connected: boolean;
     constructor(options: Options = {}) {
         let connectionName = "mqtt://localhost:1883";
         if (options.host) {
             connectionName = `mqtt://${options.host}`;
         }
-        super({
-            connectionName
-        });
+        super(SRV_PC_MONITOR);
 
         this.topic = options.topic || "jacdac";
 
@@ -37,15 +36,21 @@ class MQTTServer extends CloudAdapterServer {
             this.client.subscribe(this.topic);
         });
         this.client.on('message', (topic, message) => {
-            // post EVT_JSON: 0x80 to jacdac
             const json = message.toString();
             this.sendEvent(0x80, jdpack("z s", [topic, json]));
         });
-        this.on(UPLOAD_JSON, ({json}) => {
-            this.client.publish(this.topic, JSON.stringify(json))
+        this.addCommand(0x80,(packet)=>{
+            if(!this.connected) return
+            const [topic,message] = jdunpack(packet.data, "z s");
+            this.topic = topic;
+            this.client.subscribe(topic);
+            this.client.publish(topic, message);
         })
-        this.on(UPLOAD_BIN, ({data}) => {
-            this.client.publish(this.topic, data)
+        this.addCommand(0x81,(packet)=>{
+            if(!this.connected) return
+            const [topic,message] = jdunpack(packet.data, "z s");
+            this.topic = topic;
+            this.client.publish(topic, message)
         })
     }
 
